edfc265b6df359b6f2793452d1ddc04cafdfdce4
hwngnt <hwng.nt@outlook.com>
2023-04-09 18:06:08 +0700

    first commit

---
+++ product-service/src/main/java/com/example/projectservice/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.projectservice.controller;
+
+import com.example.projectservice.dto.ProductRequest;
+import com.example.projectservice.dto.ProductResponse;
+import com.example.projectservice.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
